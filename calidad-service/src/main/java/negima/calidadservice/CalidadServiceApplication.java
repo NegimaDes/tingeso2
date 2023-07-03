8b7976b897152ee6e4abffaf78f4624fdba70ce2
Negima <91228411+NegimaDes@users.noreply.github.com>
2023-07-03 19:41:45 -0400

    cambio de nombres servicios

---
+++ calidad-service/src/main/java/negima/calidadservice/CalidadServiceApplication.java
@@ -0,0 +1,15 @@
+package negima.calidadservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class CalidadServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalidadServiceApplication.class, args);
+	}
+
+}
---
+++ calidad-service/src/test/java/negima/calidadservice/CalidadServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package negima.calidadservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalidadServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
